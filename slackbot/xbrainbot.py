b52c138de78f360aea50fb9b043c4854e9a3c959
cpk42 <cpkrommenhoek@yahoo.com>
2018-08-28 13:09:53 -0700

    added training data and cleaned up slackbot

---
+++ slackbot/xbrainbot.py
@@ -0,0 +1,75 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    xbrainbot.py                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ckrommen <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/08/27 16:58:08 by ckrommen          #+#    #+#              #
+#    Updated: 2018/08/27 16:58:46 by ckrommen         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import os
+import time
+import re
+from slackclient import SlackClient
+
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+xbrainbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+"""
+Parses a list of events coming from the Slack RTM API to find bot commands.
+If a bot command is found, this function returns a tuple of command and channel.
+If its not found, then this function returns None, None.
+"""
+def parse_bot_commands(slack_events):
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == xbrainbot_id:
+                return message, event["channel"]
+    return None, None
+
+    """
+    Finds a direct mention (a mention that is at the beginning) in message text
+    and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+def parse_direct_mention(message_text):
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username, the second group contains the remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+    response = None
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
+
+
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        xbrainbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
