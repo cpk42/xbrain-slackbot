7c0c399765cb16120598f7a487a8c925bdc4f3ad
cpk42 <cpkrommenhoek@yahoo.com>
2018-08-27 21:38:32 -0700

    trying to make progress, instructions are unclear and i dont have a partner :/ need more research

--- xbrain/xbrainbot.py
+++ xbrain/xbrainbot.py
@@ -26,12 +26,12 @@ RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
+"""
+Parses a list of events coming from the Slack RTM API to find bot commands.
+If a bot command is found, this function returns a tuple of command and channel.
+If its not found, then this function returns None, None.
+"""
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
@@ -39,19 +39,23 @@ def parse_bot_commands(slack_events):
                 return message, event["channel"]
     return None, None
 
+    """
+    Finds a direct mention (a mention that is at the beginning) in message text
+    and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+
+
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
     matches = re.search(MENTION_REGEX, message_text)
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+    """
+    Executes bot command if the command is known
+    """
+
+
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
     # Default response is help text for the user
     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
@@ -67,6 +71,7 @@ def handle_command(command, channel):
         channel=channel,
         text=response or default_response
     )
+
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
