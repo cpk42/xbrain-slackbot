ff9e26e42c972e63f67421dcf4a2f2b6c5c6f601
cpk42 <cpkrommenhoek@yahoo.com>
2018-08-28 23:22:06 -0700

    linked slackbot to lambda

--- slackbot/xbrainbot.py
+++ slackbot/xbrainbot.py
@@ -13,24 +13,23 @@
 import os
 import time
 import re
+import requests
 from slackclient import SlackClient
 
-
-# instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-# starterbot's user ID in Slack: value is assigned after the bot starts up
 xbrainbot_id = None
 
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
+URL = "https://1mg1v0jpv0.execute-api.us-west-2.amazonaws.com/dev/xbrain-handler/"
+GETR = "http://localhost/"
+RTM_READ_DELAY = 1
+EXAMPLE_COMMAND = "hello"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+data= None
 
-"""
-Parses a list of events coming from the Slack RTM API to find bot commands.
-If a bot command is found, this function returns a tuple of command and channel.
-If its not found, then this function returns None, None.
-"""
+#Parses a list of events coming from the Slack RTM API to find bot commands.
+#If a bot command is found, this function returns a tuple of command and channel.
+#If its not found, then this function returns None, None.
+
 def parse_bot_commands(slack_events):
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
@@ -39,27 +38,32 @@ def parse_bot_commands(slack_events):
                 return message, event["channel"]
     return None, None
 
-    """
-    Finds a direct mention (a mention that is at the beginning) in message text
-    and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
+
 def parse_direct_mention(message_text):
     matches = re.search(MENTION_REGEX, message_text)
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+
 def handle_command(command, channel):
     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
     response = None
+    data = {"event":{"text":command,"channel":channel}}
+    # Need to verify that the command passed in is referenced from the dataset, if so
+    # send a POST request to the lambda server with the query, and return the solution
+    # with a get request
+    # ------Must be done after model data is stored somewhere useable such as
+    # ------MySQLlite databse or clour server
     if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
-
+            response = "Suh dude"
+            r = requests.post(url=URL, json=data)
+            print(r.status_code, r.reason, r.json())
+    else:
+        slack_client.api_call (
+            "chat.postMessage",
+            channel=channel,
+            text=response or default_response
+        )
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
